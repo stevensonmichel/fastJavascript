5851d79f71f6443cc62d2e9be61aacdb51011ca4
Stevenson <michels@berea.edu>
2024-04-26 08:12:04 -0400

    Populated the products section

--- main.js
+++ main.js
@@ -1,62 +1,252 @@
-// alert("What the hell bro!!!")
-let accountNumber = "PT46738363"
-let sliced = accountNumber.slice(0, 2);
-let replaced = accountNumber.replace("P", "B");
-console.log(accountNumber + " and " + sliced + " and " + replaced);
+const galleryImages = [
+    {
+        src: "./assets/gallery/image1.jpg",
+        alt: "Thumbnail Image 1"
+    },
+    {
+        src: "./assets/gallery/image2.jpg",
+        alt: "Thumbnail Image 2"
+    },
+    {
+        src: "./assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    }
+];
 
-document.querySelector("#open-nav-menu").addEventListener("click", function() {
-    document.querySelector("header nav .wrapper").classList.add("nav-open");
-})
+const products = [
+    {
+      title: "AstroFiction",
+      author: "John Doe",
+      price: 49.9,
+      image: "./assets/products/img6.png"
+    },
+    {
+      title: "Space Odissey",
+      author: "Marie Anne",
+      price: 35,
+      image: "./assets/products/img1.png"
+    },
+    {
+      title: "Doomed City",
+      author: "Jason Cobert",
+      price: 0,
+      image: "./assets/products/img2.png"
+    },
+    {
+      title: "Black Dog",
+      author: "John Doe",
+      price: 85.35,
+      image: "./assets/products/img3.png"
+    },
+    {
+      title: "My Little Robot",
+      author: "Pedro Paulo",
+      price: 0,
+      image: "./assets/products/img5.png"
+    },
+    {
+      title: "Garden Girl",
+      author: "Ankit Patel",
+      price: 45,
+      image: "./assets/products/img4.png"
+    }
+  ]
 
-document.querySelector("#close-nav-menu").addEventListener("click", function() {
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");
-})
+// Menu Section
+function menuHandler() {
+    document.querySelector("#open-nav-menu").addEventListener("click", function() {
+        document.querySelector("header nav .wrapper").classList.add("nav-open");
+    });
+    
+    document.querySelector("#close-nav-menu").addEventListener("click", function() {
+        document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    });
+}
 
+// Greeting Section
+function greetingHandler() {
+    let greetingText;
+    let currentHours = new Date().getHours();
 
-// var greetingText = ["Good Morning!", "Good Afternoon!", "Good Night!"];
+    if (currentHours < 12) {
+        greetingText = "Good Morning!";
+    } else if (currentHours < 19) {
+        greetingText = "Good Afternoon!";
+    } else if (currentHours < 24) {
+        greetingText = "Good Evening!";
+    } else {
+        greetingText = "Welcome!";
+    }
 
-// var time = 89;
+    const weatherCondition = "sunny";
+    const userLocation = "Mumbai";
+    let temperature = 35.78;
 
-// function greetingDisplay() {
-//     if (hours > 18) {
-//         document.getElementById("greeting").innerHTML = greetingText[3]
-//     } 
-// }
+    let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)} fahrenheit outside`
+    let fahrenheitText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)} fahrenheit outside`
 
+    document.querySelector("#greeting").innerHTML = greetingText;
+    document.querySelector("p#weather").innerHTML = celsiusText;
 
+    document.querySelector(".weather-group").addEventListener("click", function(e) {
+        temp = e.target.id;
+        if (temp == "celsius") {
+            document.querySelector("p#weather").innerHTML = celsiusText;
+        }
+        else {
+            document.querySelector("p#weather").innerHTML = fahrenheitText;
+        }
+    });
+}
+
+// Converter Section
 function celsiusToFahr(temperature) {
     let = fahr = (temperature * 9/5) + 32;
     return fahr;
 }
 
+// Clock Section
+function clockHandler() {
+    setInterval(function() {
+        let localTime = new Date();
+        document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, "0");
+        document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, "0");
+        document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, "0");
+    }, 1000);
+}
 
-const greetingText = "Good AfterMorning!";
-const weatherCondition = "sunny";
-const userLocation = "Mumbai";
-let temperature = 35.78;
+// let animals = ["dog", "cat", "cow", "elephant", "lion"]
+
+// for (let a = 0; a < animals.length; a++) {
+//     console.log(animals[a]);
+// }
+
+// for (let a in animals) {
+//     console.log(animals[a]);
+// }
+
+// Gallery Section
+function galleryHandler() {
+    let mainImage = document.querySelector("#gallery > img");
+    let thumbnails = document.querySelector("#gallery .thumbnails");
+
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
+
+    galleryImages.forEach(function(image, index) {
+        let thumb = document.createElement("img");
+        thumb.src = image.src;
+        thumb.alt = image.alt;
+        thumb.dataset.arrayIndex = index;
+        thumb.dataset.selected = index === 0 ? true : false;
+        thumb.addEventListener("click", function(e) {
+            let selectedIndex = e.target.dataset.arrayIndex;
+            let selectedImage = galleryImages[selectedIndex];
+            mainImage.src = galleryImages[selectedIndex].src;
+            mainImage.alt = galleryImages[selectedIndex].alt;
+            thumbnails.querySelectorAll("img").forEach(function(img) {
+                img.dataset.selected = false;
+            })
+            e.target.dataset.selected = true;
+        })
+        thumbnails.appendChild(thumb);
+
+    // if (index === 0) {
+    //     thumb.dataset.selected = true;
+    // }
+    // else {
+    //     thumb.dataset.selected = false;
+    // }
+    })
+}
 
 
-let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)} fahrenheit outside`
-let fahrenheitText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)} fahrenheit outside`
+/* for (let i in galleryImages) {
+    console.log(galleryImages[i]);
+}
+ */
 
-document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = celsiusText;
 
-document.querySelector(".weather-group").addEventListener("click", function(e) {
-    console.log(e.target.id)
-    temp = e.target.id;
-    if (temp == "celsius") {
-        document.querySelector("p#weather").innerHTML = celsiusText;
-    }
-    else {
-        document.querySelector("p#weather").innerHTML = fahrenheitText;
-    }
+
+// Products Section
+/* <div class="product-item">
+    <img src="./assets/products/img1.png" alt="Space Odissey">
+    <div class="product-details">
+        <h3 class="product-title">Space Odissey</h3>
+        <p class="product-author">Marie Anne</p>
+        <p class="price-title">Price</p>
+        <p class="product-price">$ 35.00</p>
+    </div>
+</div> */
+
+
+function productsHandler() {
+    let productSection = document.querySelector(".products-area");
+    let freeProducts = products.filter(function(item) {
+        return !item.price || item.price <= 0;
+    });
+    let paidProducts = products.filter(function(item) {
+        return item.price > 0;
+    });
+
+    products.forEach(function(product, index) {
+        // Create the html for the individual image
+        let productElement = document.createElement("div");
+        productElement.classList.add("product-item");
+        
+        // Create the product image
+        let productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = "Image for " + product.title;
+
+        // Create the product detail section
+        let productDetails = document.createElement("div");
+        productDetails.classList.add("product-details");
+
+        // Create product title, author, price-title, and price
+        let productTitle = document.createElement("h3");
+        productTitle.classList.add("product-title");
+        productTitle.textContent = product.title;
+
+        let productAuthor = document.createElement("p");
+        productAuthor.classList.add("product-author");
+        productAuthor.textContent = product.author;
+
+        let productPriceTitle = document.createElement("p");
+        productPriceTitle.classList.add("price-title");
+        productPriceTitle.textContent = "Price";
+
+        let productPrice = document.createElement("p");
+        productPrice.classList.add("product-price");
+        productPrice.textContent = product.price > 0 ? "$ " + product.price.toFixed(2) : "Free";
+
+
+        // Append the product details
+        productDetails.append(productTitle);
+        productDetails.append(productAuthor);
+        productDetails.append(productPriceTitle);
+        productDetails.append(productPrice);
+
+        productElement.append(productImage);
+        productElement.append(productDetails);
+
+        productSection.append(productElement)
+    });
+
     
-});
+    document.querySelector(".products-filter label[for=all] span.product-amount").textContent = products.length; 
+    document.querySelector(".products-filter label[for=paid] span.product-amount").textContent = paidProducts.length;
+    document.querySelector(".products-filter label[for=free] span.product-amount").textContent = freeProducts.length;
+}
 
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// let greaterThan4 = numbers.filter(function(item) {
+//     return item > 5;
+// })
 
-// new Date().getHours();
-
-// new Date().getMinutes();
-
-// new Date().getSeconds();
+// Page Load
+menuHandler();
+greetingHandler();
+clockHandler();
+galleryHandler();
+productsHandler();
